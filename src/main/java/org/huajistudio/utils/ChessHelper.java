9e08cac0edc01ec0554dffe40320b8d132b7bdb9
mzhmxzh <jackzhang0906@126.com>
2017-05-13 22:26:41 +0800

    Special judgement for Pawn movements

--- src/main/java/org/huajistudio/utils/ChessHelper.java
+++ src/main/java/org/huajistudio/utils/ChessHelper.java
@@ -5,13 +5,36 @@ import org.huajistudio.api.Chess;
 import org.huajistudio.api.ChessObject;
 
 class ChessHelper {
+
+	public static final boolean BLACK = true;
+	public static final boolean WHITE = false;
+
     public static boolean CanMoveTo(ChessObject chess, Board.BoardPos pos, Board board) {
-    	ChessObject chessObjectAt = board.chessObjects.get(pos);
-    	if (chessObjectAt != null)
-    		if (chessObjectAt.side == chess.side)
-    			return false;
-    	if (chess.type == Chess.PAWN)
-    		return true; // TODO special judge
+    	ChessObject chessObjectAtDestination = board.chessObjects.get(pos);
+    	if (chessObjectAtDestination != null)
+		{
+			if (chessObjectAtDestination.side == chess.side)
+			{
+				return false;
+			}
+		}
+
+
+		if (chess.type == Chess.PAWN &&
+			chess.side==BLACK &&
+			chess.pos.y==1 &&
+			pos.x==chess.pos.x&&pos.y==3)
+		{
+			return true;
+		}
+		if (chess.type == Chess.PAWN &&
+			chess.side==WHITE &&
+			chess.pos.y==6 &&
+			pos.x==chess.pos.x&&pos.y==4)
+		{
+			return true;
+		}
+
     	return chess.type.getMoves().contains(pos);
     }
 }
