eca7e6ae3ccaf5a113fa433ef88092908a1a88a0
Lasm_Gratel <lasm_gratel@hotmail.com>
2017-05-15 20:15:46 +0800

    re-written BoardReader

--- src/main/java/org/huajistudio/file/BoardReader.java
+++ src/main/java/org/huajistudio/file/BoardReader.java
@@ -1,37 +1,34 @@
 package org.huajistudio.file;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import org.apache.commons.io.FileUtils;
 import org.huajistudio.ChessMaster;
 import org.huajistudio.api.Board;
-import org.huajistudio.api.BoardObject;
+import org.huajistudio.api.config.BoardSerializer;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
+import java.util.Optional;
 import java.util.zip.GZIPInputStream;
 
 public class BoardReader {
-	public static Board readBoard(File file) {
-		Gson gson = new Gson();
-		BoardObject boardObject = new BoardObject();
-		if (file.toString().endsWith(".gz")) {
-			try {
-				boardObject = gson.fromJson(new InputStreamReader(new GZIPInputStream(new FileInputStream(file)), StandardCharsets.UTF_8), BoardObject.class);
-			} catch (IOException e) {
-				ChessMaster.getLogger().error("Cannot read gzipped board!", e);
-			}
+	public static Optional<Board> readBoard(File file) {
+		Gson gson = new GsonBuilder().registerTypeAdapter(Board.class, new BoardSerializer()).create();
+		Board board = null;
+		if (file.toString().endsWith(".gz")) try {
+			board = gson.fromJson(new InputStreamReader(new GZIPInputStream(new FileInputStream(file)), StandardCharsets.UTF_8), Board.class);
+		} catch (IOException e) {
+			ChessMaster.getLogger().error("Cannot read gzipped board!", e);
 		}
 		try {
-			boardObject = gson.fromJson(FileUtils.readFileToString(file, StandardCharsets.UTF_8), BoardObject.class);
+			board = gson.fromJson(FileUtils.readFileToString(file, StandardCharsets.UTF_8), Board.class);
 		} catch (IOException e) {
 			ChessMaster.getLogger().error("Cannot read board!", e);
 		}
-		Board board = new Board();
-		Arrays.stream(boardObject.getChessObjects()).forEach(board::putChess);
-		return board;
+		return Optional.ofNullable(board);
 	}
 }
