f129707320d616a80cb824e12ebe93ff3c41f9ef
mzhmxzh <jackzhang0906@126.com>
2017-05-16 22:30:41 +0800

    Special judgement for King-Castle Castling And a need-to-be-shortened method that checks whether a ChessObject is under the threat of anothor

--- src/main/java/org/huajistudio/utils/ChessHelper.java
+++ src/main/java/org/huajistudio/utils/ChessHelper.java
@@ -2,6 +2,7 @@ package org.huajistudio.utils;
 
 import org.huajistudio.api.*;
 import org.huajistudio.api.math.ChessVector;
+import java.util.Iterator;
 
 public interface ChessHelper {
 	boolean BLACK = true;
@@ -46,6 +47,369 @@ public interface ChessHelper {
 		    else
 			    return chess.getPos().move(0, -1).equals(pos);
 
+                        //Castling
+            if (chess.getType() == Chess.KING)
+            {
+                if (chess.isMoved() == false)
+                {
+                    if (chessObjectAtDestination.getSide() == chess.getSide() && 
+                        chessObjectAtDestination.getType() == Chess.CASTLE && 
+                        chessObjectAtDestination.isMoved() == false)
+                    {
+                        //Check if there are things blocking the way
+                        int start = chess.getPos().getX();
+                        int end = chessObjectAtDestination.getPos().getX();
+                        if (start > end)
+                        {
+                            //Swap
+                            start = start + end;
+                            end = start - end;
+                            start = start - end;
+                        }
+                        start++;
+                        end--;
+                        for(int i = start; i <= end; i++)
+                        {
+                            BoardPos between = new BoardPos(i,chess.getPos().getY());
+                            if(board.getChess(between) != null)
+                            {
+                                return false;
+                            }
+                        }
+                        
+                        //Check if the king is threatened
+                        Iterator chessObjectsOnBoard = board.iterator();
+                        while (chessObjectsOnBoard.hasNext())
+                        {
+                            ChessObject aChessObjectOnBoard = (ChessObject) chessObjectsOnBoard.next();
+                            if (aChessObjectOnBoard.getSide() == chess.getSide())
+                            {
+                                continue;
+                            }
+                            if (threatenBy(chess,aChessObjectOnBoard,board) == true)
+                            {
+                                return false;
+                            }
+                        }
+                        
+                        //Dear king : If U R reading this, then U R free to do the castling
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
 		return false;
+    }
+    
+        static boolean threatenBy(ChessObject chessOne, ChessObject chessTwo, Board board)
+    {
+        BoardPos posOne = chessOne.getPos();
+        BoardPos posTwo = chessTwo.getPos();
+        if (chessTwo.getType() == Chess.CASTLE)
+        {
+            if (posOne.getX() == posTwo.getX())
+            {
+                int start = posOne.getY();
+                int end = posTwo.getY();
+                if (start == end - 1 || start == end + 1)
+                {
+                    return true;
+                }
+                else if (start < end)
+                {
+                    start++;
+                    end--;
+                }
+                else if (start > end)
+                {
+                    //Swap the two elements
+                    start = start + end;
+                    end = start - end;
+                    start = start - end;
+                    start++;
+                    end--;
+                }
+                for(int i = start; i <= end; i++)
+                {
+                    BoardPos pos = new BoardPos(posOne.getX(),i);
+                    if(board.getChess(pos) != null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if(posOne.getY() == posTwo.getY())
+            {
+                int start = posOne.getX();
+                int end = posTwo.getX();
+                if (start == end - 1 || start == end + 1)
+                {
+                    return true;
+                }
+                else if (start < end)
+                {
+                    start++;
+                    end--;
+                }
+                else if (start > end)
+                {
+                    //Swap the two elements
+                    start = start + end;
+                    end = start - end;
+                    start = start - end;
+                    start++;
+                    end--;
+                }
+                for(int i = start; i <= end; i++)
+                {
+                    BoardPos pos = new BoardPos(posOne.getX(),i);
+                    if(board.getChess(pos) != null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;//If the brave chessOne survives the bombardment of this code, then he is worthy to achieve the truth
+        }
+        else if (chessTwo.getType() == Chess.KING)
+        {
+            return Math.abs(posOne.getX()-posTwo.getX()) <= 1 && Math.abs(posOne.getY()-posTwo.getY()) <= 1;
+        }
+        else if (chessTwo.getType() == Chess.KNIGHT)
+        {
+            return (Math.abs(posOne.getX()-posTwo.getX()) == 2 && Math.abs(posOne.getY() - posTwo.getY()) == 1) || 
+                    (Math.abs(posOne.getX()-posTwo.getX()) == 1 && Math.abs(posOne.getY() - posTwo.getY()) == 2);
+        }
+        else if (chessTwo.getType() == Chess.PAWN)
+        {
+            if (chessTwo.getSide() == BLACK)
+            {
+                return chessTwo.getPos().move(1, -1).equals(posOne) || chessTwo.getPos().move(-1, -1).equals(posOne);
+            }
+            else
+            {
+                return chessTwo.getPos().move(1, 1).equals(posOne) || chessTwo.getPos().move(-1, 1).equals(posOne);
+            }
+        }
+        else if (chessTwo.getType() == Chess.POPE)
+        {
+            if (posOne.getX() + posOne.getY() == posTwo.getX() + posTwo.getY())
+            {
+                int diagonal = posOne.getX() + posOne.getY();
+                int startX = posOne.getX();
+                int endX = posTwo.getX();
+                if (startX == endX - 1 || startX == endX + 1)
+                {
+                    return true;
+                }
+                else if (startX < endX)
+                {
+                    startX++;
+                    endX--;
+                }
+                else if (startX > endX)
+                {
+                    //Swap the two elements
+                    startX = startX + endX;
+                    endX = startX - endX;
+                    startX = startX - endX;
+                    startX++;
+                    endX--;
+                }
+                for(int i = startX; i <= endX; i++)
+                {
+                    BoardPos pos = new BoardPos(i,diagonal - i);
+                    if(board.getChess(pos) != null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if(posOne.getX() - posOne.getY() == posTwo.getX() - posTwo.getY())
+            {
+                int diagonal = posOne.getX() - posOne.getY();
+                int startX = posOne.getX();
+                int endX = posTwo.getX();
+                if (startX == endX - 1 || startX == endX + 1)
+                {
+                    return true;
+                }
+                else if (startX < endX)
+                {
+                    startX++;
+                    endX--;
+                }
+                else if (startX > endX)
+                {
+                    //Swap the two elements
+                    startX = startX + endX;
+                    endX = startX - endX;
+                    startX = startX - endX;
+                    startX++;
+                    endX--;
+                }
+                for(int i = startX; i <= endX; i++)
+                {
+                    BoardPos pos = new BoardPos(i,i - diagonal);
+                    if(board.getChess(pos) != null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;//If the brave chessOne survives the bombardment of this code, then he is worthy to achieve the truth
+        }
+        else if (chessTwo.getType() == Chess.QUEEN)
+        {
+            //Ready? Go!
+            if (posOne.getX() == posTwo.getX())
+            {
+                int start = posOne.getY();
+                int end = posTwo.getY();
+                if (start == end - 1 || start == end + 1)
+                {
+                    return true;
+                }
+                else if (start < end)
+                {
+                    start++;
+                    end--;
+                }
+                else if (start > end)
+                {
+                    //Swap the two elements
+                    start = start + end;
+                    end = start - end;
+                    start = start - end;
+                    start++;
+                    end--;
+                }
+                for(int i = start; i <= end; i++)
+                {
+                    BoardPos pos = new BoardPos(posOne.getX(),i);
+                    if(board.getChess(pos) != null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if(posOne.getY() == posTwo.getY())
+            {
+                int start = posOne.getX();
+                int end = posTwo.getX();
+                if (start == end - 1 || start == end + 1)
+                {
+                    return true;
+                }
+                else if (start < end)
+                {
+                    start++;
+                    end--;
+                }
+                else if (start > end)
+                {
+                    //Swap the two elements
+                    start = start + end;
+                    end = start - end;
+                    start = start - end;
+                    start++;
+                    end--;
+                }
+                for(int i = start; i <= end; i++)
+                {
+                    BoardPos pos = new BoardPos(posOne.getX(),i);
+                    if(board.getChess(pos) != null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if (posOne.getX() + posOne.getY() == posTwo.getX() + posTwo.getY())
+            {
+                int diagonal = posOne.getX() + posOne.getY();
+                int startX = posOne.getX();
+                int endX = posTwo.getX();
+                if (startX == endX - 1 || startX == endX + 1)
+                {
+                    return true;
+                }
+                else if (startX < endX)
+                {
+                    startX++;
+                    endX--;
+                }
+                else if (startX > endX)
+                {
+                    //Swap the two elements
+                    startX = startX + endX;
+                    endX = startX - endX;
+                    startX = startX - endX;
+                    startX++;
+                    endX--;
+                }
+                for(int i = startX; i <= endX; i++)
+                {
+                    BoardPos pos = new BoardPos(i,diagonal - i);
+                    if(board.getChess(pos) != null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else if(posOne.getX() - posOne.getY() == posTwo.getX() - posTwo.getY())
+            {
+                int diagonal = posOne.getX() - posOne.getY();
+                int startX = posOne.getX();
+                int endX = posTwo.getX();
+                if (startX == endX - 1 || startX == endX + 1)
+                {
+                    return true;
+                }
+                else if (startX < endX)
+                {
+                    startX++;
+                    endX--;
+                }
+                else if (startX > endX)
+                {
+                    //Swap the two elements
+                    startX = startX + endX;
+                    endX = startX - endX;
+                    startX = startX - endX;
+                    startX++;
+                    endX--;
+                }
+                for(int i = startX; i <= endX; i++)
+                {
+                    BoardPos pos = new BoardPos(i,i - diagonal);
+                    if(board.getChess(pos) != null)
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;//LOL
+        }
+        return true;
     }
 }
